51f6d05d674cd7a2626da122a8f3aa0e98b902e2
Gonzalo Perea <gonzalopereabartra@gmail.com>
2023-11-23 19:07:59 -0500

    Added tests

--- implementacion-alterna/main.cpp
+++ implementacion-alterna/main.cpp
@@ -5,9 +5,20 @@
 #import "quadtree.h"
 #import <iostream>
 #import <fstream>
+#import <chrono>
 
 int main(){
+
+    // Initialize chrono
+    std::chrono::steady_clock::time_point begin;
+    std::chrono::steady_clock::time_point end;
+    std::chrono::duration<double> time_span;
+
     //Test the kdtree
+
+    // Start chrono
+    begin = std::chrono::steady_clock::now();
+
     KDTreeNode* root = nullptr;
     int point1[2] = {1, 2};
     int point2[2] = {3, 4};
@@ -37,9 +48,58 @@ int main(){
     else
         std::cout << "Did not find point (1, 2)" << std::endl;
 
+    //End chrono
+    end = std::chrono::steady_clock::now();
+
+    // Calculate time
+    time_span = std::chrono::duration_cast<std::chrono::duration<double>>(end - begin);
+
+    // Print time
+    std::cout << "Time: " << time_span.count() << " seconds." << std::endl;
+
+
     //test quadtree
 
+    // Start chrono
+    begin = std::chrono::steady_clock::now();
 
+    QuadTreeNode* root2 = nullptr;
+    int point8[2] = {1, 2};
+    int point9[2] = {3, 4};
+    int point10[2] = {5, 6};
+    int point11[2] = {7, 8};
+    int point12[2] = {9, 10};
+
+    root2 = Quadtree::insert(root2, point8);
+    root2 = Quadtree::insert(root2, point9);
+    root2 = Quadtree::insert(root2, point10);
+    root2 = Quadtree::insert(root2, point11);
+    root2 = Quadtree::insert(root2, point12);
+
+    Quadtree::display(root2, 0);
+
+    //test nearest neighbor
+    int point13[2] = {2, 3};
+    QuadTreeNode* nearest2 = Quadtree::nearestNeighbour(root2, point13);
+    std::cout << "Nearest neighbor to (2, 3): (" << nearest2->dataPoint[0] << ", " << nearest2->dataPoint[1] << ")" << std::endl;
+
+    //test search
+    int point14[2] = {1, 2};
+    bool found2 = Quadtree::search(root2, point14);
+
+    if (found2)
+        std::cout << "Found point (1, 2)" << std::endl;
+    else
+        std::cout << "Did not find point (1, 2)" << std::endl;
+
+    //End chrono
+    end = std::chrono::steady_clock::now();
+
+    // Calculate time
+    time_span = std::chrono::duration_cast<std::chrono::duration<double>>(end - begin);
+
+    // Print time
+    std::cout << "Time: " << time_span.count() << " seconds." << std::endl;
 
     return 0;
 }
